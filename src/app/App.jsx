0754881611ac2d7d9676d6e3d632217c56c87217
Nurbai-08 <maratovnurbai7@gmail.com>
2026-05-22 20:28:12 +0600

    сделал video card для главной странницы

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import YouTubeHeader from "../components/YouTubeHeader";
 import Sidebar from "../components/Sidebar";
+import HomePage from '../components/Video/HomePage'
 
 const App = () => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(true);
@@ -17,8 +18,7 @@ const App = () => {
 
         {}
         <main className="flex-1 overflow-y-auto text-white p-4">
-          <h2 className="text-xl mb-4">Здесь будут карточки</h2>
-          {}
+          <HomePage/>
         </main>
       </div>
     </div>
