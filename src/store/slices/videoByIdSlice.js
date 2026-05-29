b17ea2a4271ee4f7c464d70eafc97bbca7bf05a5
Renakos <renakos0129@gmail.com>
2026-05-29 23:04:58 +0600

    убрал ошибки экшенов гитхаб

--- src/store/slices/videoByIdSlice.js
+++ src/store/slices/videoByIdSlice.js
@@ -1,28 +1,28 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { fetchVideoByIdTC } from "../thunks/fetchVideoById";
 
-
 const initialState = {
-    video: null,
-    loading: false,
-    error: null
-}
+  video: null,
+  loading: false,
+  error: null,
+};
 
 const videoByIdSlice = createSlice({
-    name: "videoById",
-    initialState,
-    extraReducers: (builder) => {
-        builder.addCase(fetchVideoByIdTC.pending, (state) => {
-        state.currentVideo.loading = true;
-        state.currentVideo.error = null;
-      })
-      builder.addCase(fetchVideoByIdTC.fulfilled, (state, action) => {
-        state.currentVideo.loading = false;
-        state.currentVideo.item = action.payload.items[0]; // одно видео
-      })
-      builder.addCase(fetchVideoByIdTC.rejected, (state, action) => {
-        state.currentVideo.loading = false;
-        state.currentVideo.error = action.error.message;
-      });
-    }
-})
+  name: "videoById",
+  initialState,
+  extraReducers: (builder) => {
+    builder.addCase(fetchVideoByIdTC.pending, (state) => {
+      state.currentVideo.loading = true;
+      state.currentVideo.error = null;
+    });
+    builder.addCase(fetchVideoByIdTC.fulfilled, (state, action) => {
+      state.currentVideo.loading = false;
+      state.currentVideo.item = action.payload.items[0]; // одно видео
+    });
+    builder.addCase(fetchVideoByIdTC.rejected, (state, action) => {
+      state.currentVideo.loading = false;
+      state.currentVideo.error = action.error.message;
+    });
+  },
+});
+export default videoByIdSlice.reducer;
