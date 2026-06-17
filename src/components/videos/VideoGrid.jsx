1000f7c68beeee80bba41afb2e6bb9003b4ed079
DonyFrontend <donibro357@gmail.com>
2026-06-17 18:17:06 +0600

    исправил адаптивку

--- src/components/videos/VideoGrid.jsx
+++ src/components/videos/VideoGrid.jsx
@@ -13,7 +13,7 @@ const VideoGrid = () => {
 
   if (location.pathname === "/") {
     return (
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-3 sm:gap-4">
+      <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3   gap-3 sm:gap-4">
         {videos.map((video) => (
           <VideoCard key={video.id} video={video} />
         ))}
